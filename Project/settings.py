09fab048204d6cf4d9da15412229b31640441ace
Alauddin <a.faya@fintechsys.net>
2026-01-28 08:52:37 +0300

    .

--- Project/settings.py
+++ Project/settings.py
@@ -129,6 +129,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD','wr94$43URhE4'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
