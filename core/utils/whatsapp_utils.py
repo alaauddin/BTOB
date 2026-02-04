4639186d2e2df9f60179aa972f6529f2d78d9a37
Alauddin <a.faya@fintechsys.net>
2026-02-04 21:46:34 +0300

    feat: update product list URL path to include 'products/' prefix.

--- core/utils/whatsapp_utils.py
+++ core/utils/whatsapp_utils.py
@@ -1,9 +1,31 @@
 import requests
 import logging
 import threading
+import ssl
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
+
 from core.models import SystemSettings
 
 logger = logging.getLogger(__name__)
+
+class LegacySSLAdapter(HTTPAdapter):
+    """
+    Au custom HTTP adapter that lowers the SSL security level to allow weak ciphers.
+    This fixes 'SSLEOFError' on some legacy or misconfigured servers.
+    """
+    def init_poolmanager(self, connections, maxsize, block=False):
+        context = ssl_.create_urllib3_context()
+        # Set security level to 1 (allows legacy ciphers/protocols)
+        # This is often needed for servers that don't support the stricter defaults of OpenSSL 3.0+
+        context.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_context=context
+        )
 
 def _send_whatsapp_message_sync(phone, message):
     """
@@ -24,7 +46,12 @@ def _send_whatsapp_message_sync(phone, message):
              phone = '967' + phone
 
     try:
-        response = requests.post(
+        session = requests.Session()
+        # Mount the custom adapter for the WhatsApp API URL
+        adapter = LegacySSLAdapter()
+        session.mount('https://', adapter)
+
+        response = session.post(
             system_settings.whatsapp_api_url,
             headers={
                 'X-API-Key': system_settings.whatsapp_api_key,
