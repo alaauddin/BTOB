3198cb1da9954195a3e54ce300983e724b1fa2c4
Alauddin <a.faya@fintechsys.net>
2026-02-26 00:31:07 +0300

    .

--- core/context_processors.py
+++ core/context_processors.py
@@ -61,7 +61,7 @@ def system_settings(request):
 
     return {
         'system_settings': settings,
-        'css_version': '1.14.0', # Bump version for Meta Pixel integration
+        'css_version': '1.15.0', # Bump version for Meta Pixel integration
         'nav_state': nav_state,
         'active_store': active_store,
         'user_stores_count': user_stores_count,
